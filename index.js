b40cd161c54c7b1528f78d209ed8417027807419
Israelí Pérez <israeli.ps@gamil.com>
2021-11-05 20:05:17 -0600

    update

--- index.js
+++ index.js
@@ -1,24 +1,27 @@
 const express = require("express");
-const apiRouter = require("./routes");
 const config = require("./lib/config");
+const cors = require("cors");
+const app = express();
+
+const apiRouter = require("./routes");
+
 const { logErrors, errorHandler } = require("./middlewares/errorHandlers");
 const authHandler = require("./middlewares/authHandlers");
-const cors = require("cors");
-const db = require("./lib/db");
 
-const app = express();
-const port = config.app.port;
 
 app.use(cors());
+const db = require("./lib/db");
+
+
+const port = config.app.port;
 
 app.use(express.json());
-
 // app.get("/", (request, response) => {
 //   response.send("Hello World!");
 // });
 
 apiRouter(app);
-console.log("iniciando")
+
 // app.use(logErrors);
 // app.use(errorHandler);
 
