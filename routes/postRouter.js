09c4b687ac488c6868d142d0af216696f09695b5
Israelí Pérez <israeli.ps@gamil.com>
2021-11-07 17:38:03 -0600

    sstart node

--- routes/postRouter.js
+++ routes/postRouter.js
@@ -6,88 +6,75 @@ const { authHandler, userHandler } = require("../middlewares/authHandlers");
 
 const router = express.Router();
 
-  
 router.get("/", async (req, res, next) => {
-    //posts/?search=
-    //posts/?date=
-       
-   const {search} = req.query
-   const {date} = req.query
+  //posts/?search=
+  //posts/?date=
 
-   console.log("search req.query:",search)
-   console.log("date req.query:",date)
-   try {
-    
-        const postRetrieve = await post.get(search,date)
-        res.status(200).json({
-            ok:true,
-            message:`Posts retrieved`,
-            payload:{
-                postRetrieve,
-            }
-        })
-   }catch (err) {
-     next(err);
-     console.log(err);
-   }
- });
+  const { search } = req.query;
+  const { date } = req.query;
 
-
-  
-router.get("/:id", async (req, res, next) => {
-  const {id} = req.params
+  console.log("search req.query:", search);
+  console.log("date req.query:", date);
   try {
-    const postId = await post.getById(id)
-       if(postId){
-          res.status(200).json({
-            ok:true,
-            message:`Post {id} retrieved`,
-            payload:{
-                postId,
-          }
-        })
-        }else{
-          res.status(404).json({
-            ok:false,
-            message:`Post id not found`,
-            payload:{
-                postId,
-            }
-          })
-        }
-   }catch (err) {
-     next(err);
-     console.log(err);
+    const postRetrieve = await post.get(search, date);
+    res.status(200).json({
+      ok: true,
+      message: `Posts retrieved`,
+      payload: postRetrieve,
+    });
+  } catch (err) {
+    next(err);
+    console.log(err);
+  }
+});
+
+router.get("/:id", async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const postId = await post.getById(id);
+    if (postId) {
+      res.status(200).json({
+        ok: true,
+        message: `Post {id} retrieved`,
+        payload: {
+          postId,
+        },
+      });
+    } else {
+      res.status(404).json({
+        ok: false,
+        message: `Post id not found`,
+        payload: {
+          postId,
+        },
+      });
     }
+  } catch (err) {
+    next(err);
+    console.log(err);
+  }
 });
 
 router.use(authHandler);
 
 router.post("/", async (req, res, next) => {
-    const {token} = req.headers
-    
-    const dataPost = req.body
+  const { token } = req.headers;
 
-    const payload = await jwt.verifyToken(token)
-    
-    const {sub} = payload
-    const userObject = await user.getById(sub)    
-    const userName = userObject.username
-   
-    try {
-        
-       const postCreated = await post.create(dataPost,userName)
-    } catch (err) {
-        next(err);
-        console.log(err);
-    }
-    
-    
-})   
+  const dataPost = req.body;
 
+  const payload = await jwt.verifyToken(token);
 
+  const { sub } = payload;
+  const userObject = await user.getById(sub);
+  const userName = userObject.username;
 
-
+  try {
+    const postCreated = await post.create(dataPost, userName);
+  } catch (err) {
+    next(err);
+    console.log(err);
+  }
+});
 
 // Usamos userhHandler para que solo el usuario puede modificar su propio registro
 router.patch("/:id", userHandler, async (req, res, next) => {
@@ -99,29 +86,27 @@ router.patch("/:id", userHandler, async (req, res, next) => {
     if (!payload) {
       throw new Error("Post not found");
     }
-    } catch (err) {
-        next(err);
-        console.log(err);
-}
-})
-  
- 
-  
-  router.delete("/:id", async (req, res, next) => {
-      const {id}= req.params
-    try {
-        const postDel = await post.del(id)
-                res.status(200).json({
-            ok:true,
-            message:`Post ${id} deleted`,
-            payload:{
-                postDel,
-            }
-        })
-    } catch (err) {
-      next(err);
-      console.log(err);
-    }
-  });
-  
-  module.exports=router;
+  } catch (err) {
+    next(err);
+    console.log(err);
+  }
+});
+
+router.delete("/:id", async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const postDel = await post.del(id);
+    res.status(200).json({
+      ok: true,
+      message: `Post ${id} deleted`,
+      payload: {
+        postDel,
+      },
+    });
+  } catch (err) {
+    next(err);
+    console.log(err);
+  }
+});
+
+module.exports = router;
--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -1,23 +1,23 @@
 const express = require("express");
-const user = require("../usercases/user")
+const user = require("../usercases/user");
 const jwt = require("../lib/jwt");
 const bcrypt = require("bcrypt");
-const {authHandler,userHandler} = require("../middlewares/authHandlers")
+const { authHandler, userHandler } = require("../middlewares/authHandlers");
 
 const router = express.Router();
 
 router.post("/", async (req, res, next) => {
   try {
-    const userData = req.body
-    const userCreated = await user.create(userData)
-    const {username} =userCreated
+    const userData = req.body;
+    const userCreated = await user.create(userData);
+    const { username } = userCreated;
     res.status(201).json({
-        ok:true,
-        message:"User Created successfully",
-        payload:{
-            username,
-        }
-    })
+      ok: true,
+      message: "User Created successfully",
+      payload: {
+        username,
+      },
+    });
   } catch (err) {
     next(err);
     console.log(err);
@@ -26,50 +26,47 @@ router.post("/", async (req, res, next) => {
 
 // A partir de este punto se necesita token
 
-router.use(authHandler)
-
+router.use(authHandler);
 
 router.get("/:id", async (req, res, next) => {
-   const {id} = req.params
+  const { id } = req.params;
 
-    const userObject = await user.getById(id)
+  const userObject = await user.getById(id);
   try {
     res.json({
-                
-      id:userObject.id,
-      userName:userObject.name,
-    })  
-  }catch (err) {
+      id: userObject.id,
+      userName: userObject.name,
+    });
+  } catch (err) {
     next(err);
     console.log(err);
   }
 });
 
 // Usamos userhHandler para que solo el usuario puede modificar su propio registro
-router.patch("/:id",userHandler, async (req, res, next) => {
+router.patch("/:id", userHandler, async (req, res, next) => {
   try {
-    const {id}=req.params
-    const userData=req.body
+    const { id } = req.params;
+    const userData = req.body;
 
-    console.log(userData)
-    const userUpdate = await user.update(id,userData)
+    console.log(userData);
+    const userUpdate = await user.update(id, userData);
     res.status(200).json({
-      status:true,
-      message:"Update succesfull",
-      payload:{
-        userId:userUpdate._id,
-        name:userUpdate.name,
-        username:userUpdate.username,
-      }
-    })
-    
+      status: true,
+      message: "Update succesfull",
+      payload: {
+        userId: userUpdate._id,
+        name: userUpdate.name,
+        username: userUpdate.username,
+      },
+    });
   } catch (err) {
     next(err);
     console.log(err);
   }
 });
 
-router.delete("/:id",userHandler, async (req, res, next) => {
+router.delete("/:id", userHandler, async (req, res, next) => {
   try {
   } catch (err) {
     next(err);
@@ -77,4 +74,4 @@ router.delete("/:id",userHandler, async (req, res, next) => {
   }
 });
 
-module.exports=router;
+module.exports = router;
